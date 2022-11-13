f8594026bbe8241c53090647ed670d0b51c3f2b4
peggy <peggy.tsai@cathaysec.com.tw>
2022-11-13 09:57:40 +0800

    邏輯抽離

--- Tdd/TDD/BudgetService.swift
+++ Tdd/TDD/BudgetService.swift
@@ -12,43 +12,9 @@ class BudgetService {
         budgetRepo = repo
     }
 
-    private func getMonthRatio(from: Date?, to: Date?) -> Double {
-        guard let from = from, let to = to, from <= to else {
-            return 0
-        }
-        guard let days = Calendar.current.dateComponents([.day], from: from, to: to).day else {
-            return 0
-        }
-        return (days+1).double / from.getDaysInMonth().double
-    }
-
     func getBudget(startDate: Date?, endDate: Date?) -> Double {
-        guard let startDateOfYearMonth = startDate?.getDate(with: "yyyyMM"),
-              let endDateOfYearMonth = endDate?.getDate(with: "yyyyMM") else {
-            return 0
-        }
-
-        let budgets = budgetRepo?.getAll().filter {
-            guard let date = $0.getDate() else {
-                return false
-            }
-            return date >= startDateOfYearMonth && date <= endDateOfYearMonth
-        } ?? []
-
-        return budgets.reduce(0.0) {
-            guard let date = $1.getDate() else {
-                return $0
-            }
-            let amt = $1.amt.double
-            var ratio = 1.0 //預設100%
-            if startDateOfYearMonth == endDateOfYearMonth {
-                ratio = getMonthRatio(from: startDate, to: endDate)
-            } else if startDateOfYearMonth == date {
-                ratio = getMonthRatio(from: startDate, to: startDate?.getLastDayOfDate())
-            } else if endDateOfYearMonth == date {
-                ratio = getMonthRatio(from: endDateOfYearMonth, to: endDate)
-            }
-            return $0 + amt * ratio
-        }
+        budgetRepo?.getAll().reduce(0) { result, budget in
+            result + budget.getOverlayBudget(startDate: startDate, endDate: endDate)
+        } ?? 0
     }
 }
--- Tdd/TDD/IBudgetReport.swift
+++ Tdd/TDD/IBudgetReport.swift
@@ -9,8 +9,44 @@ struct Budget {
     var date: String
     var amt: Int
 
-    func getDate() -> Date? {
+    func getDateOfYearMonth() -> Date? {
         date.toDate(with: "yyyyMM")
+    }
+
+    func getDailyBudget() -> Double {
+        guard let date = getDateOfYearMonth() else {
+            return 0
+        }
+        return amt.double / date.getDaysInMonth().double
+    }
+
+    func getOverlayBudget(startDate: Date?, endDate: Date?) -> Double {
+        guard let date = getDateOfYearMonth() else{
+            return 0
+        }
+        guard let startDate = startDate, let endDate = endDate, startDate <= endDate else {
+            return 0
+        }
+        guard let firstDayOfMonth = date.getFirstDayOfDate(),
+              let lastDayOfMonth = date.getLastDayOfDate() else {
+            return 0
+        }
+        guard startDate <= lastDayOfMonth && endDate >= firstDayOfMonth else {
+            return 0
+        }
+        let start = startDate > firstDayOfMonth ? startDate : firstDayOfMonth
+        let end = endDate < lastDayOfMonth ? endDate : lastDayOfMonth
+        return getDailyBudget() * getDaysDiff(from: start, to: end).double
+    }
+
+    private func getDaysDiff(from: Date?, to: Date?) -> Int {
+        guard let from = from, let to = to else {
+            return 0
+        }
+        guard let days = Calendar.current.dateComponents([.day], from: from, to: to).day else {
+            return 0
+        }
+        return days+1
     }
 }
 
